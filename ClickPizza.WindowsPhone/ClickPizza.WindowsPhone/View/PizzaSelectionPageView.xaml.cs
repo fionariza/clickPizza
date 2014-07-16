5adb9cf8a21c160a84158e23c7077035a07745d6
fionariza <okrotowa@gmail.com>
2014-07-16 12:33:46 +0300

    Added

---
+++ ClickPizza.WindowsPhone/ClickPizza.WindowsPhone/View/PizzaSelectionPageView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ClickPizza.WindowsPhone.ViewModel;
+using Microsoft.Phone.Controls;
+
+namespace ClickPizza.WindowsPhone.View
+{
+    public partial class PizzaSelectionPageView : PhoneApplicationPage
+    {
+        // Constructor
+        public PizzaSelectionPageView()
+        {
+            InitializeComponent();
+            DataContext = new PizzaSelectionPageViewModel();
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+    }
+}
---
+++ ClickPizza.WindowsPhone/ClickPizza.WindowsPhone/ViewModel/PizzaSelectionPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+namespace ClickPizza.WindowsPhone.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class PizzaSelectionPageViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the PizzaSelectedListViewModel class.
+        /// </summary>
+        public PizzaSelectionPageViewModel()
+        {
+
+        }
+
+        private readonly ObservableCollection<PizzaDetailsViewModel> _pizzaCollection = new ObservableCollection<PizzaDetailsViewModel>();
+
+        public IEnumerable<PizzaDetailsViewModel> PizzaCollection
+        {
+            get { return _pizzaCollection; }
+        }
+
+    }
+}
