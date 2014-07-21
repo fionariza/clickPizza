69d2d62afa156b9b9f75b910cab3c1583491f781
fionariza <okrotowa@gmail.com>
2014-07-21 09:55:24 +0300

    Added PizzaNavigationPageViewModel

---
+++ ClickPizza.WindowsPhone/ClickPizza.WindowsPhone/ViewModel/PizzaNavigationPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Phone.Controls;
+
+namespace ClickPizza.WindowsPhone.ViewModel
+{
+    public class PizzaNavigationPageViewModel : ViewModelBase
+    {
+
+        public PizzaNavigationPageViewModel()
+        {
+            NavigateCommand = new RelayCommand<string>(Navigation);
+        }
+        
+        private void Navigation(string obj)
+        {
+            var phoneApplicationFrame = Application.Current.RootVisual as PhoneApplicationFrame;
+            if (phoneApplicationFrame != null)
+                phoneApplicationFrame.Navigate(new Uri("/View/"+obj, UriKind.Relative));
+        }
+
+        public RelayCommand<string> NavigateCommand { get; private set; }
+
+    }
+}
